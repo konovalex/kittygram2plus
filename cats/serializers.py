1a90d844cd1103d07b12c455086333d2af2b05e7
Konov Aleksey <konov.alexey@gmail.com>
2024-05-27 18:49:12 +0300

    Fix code according the theory

--- cats/serializers.py
+++ cats/serializers.py
@@ -27,7 +27,7 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
     age = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
@@ -45,7 +45,7 @@ class CatSerializer(serializers.ModelSerializer):
             achievements = validated_data.pop('achievements')
             cat = Cat.objects.create(**validated_data)
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
+                current_achievement, st = Achievement.objects.get_or_create(
                     **achievement)
                 AchievementCat.objects.create(
                     achievement=current_achievement, cat=cat)
--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,36 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework import filters
 from rest_framework import viewsets
 
 from .models import Achievement, Cat, User
-
+from .pagination import CatsPagination
+from .permissions import OwnerOrReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    )
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
+    # pagination_class = CatsPagination
+    # throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # throttle_scope = 'low_request'
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
